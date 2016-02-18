82e9fd7876600e9315ee4597218aab6e992e161a
KYLE KWONG <kylekwong@college.harvard.edu>
2016-02-18 03:26:19 -0500

    indentation -> spaces

--- randmst.c
+++ randmst.c
@@ -2,21 +2,20 @@
 
 // tuple structure
 typedef struct {
-	int x;
-	int y;
-	int z;
-	int a;
+    int x;
+    int y;
+    int z;
+    int a;
 } coord;
 
 int main(int argc, char* argv[]) {
-	if (argc != 5) {
-		printf("Usage: randmst 0 numpoints numtrials dimension\n");
-		return 1;
-	}
+    if (argc != 5) {
+        printf("Usage: randmst 0 numpoints numtrials dimension\n");
+        return 1;
+    }
 
-	// TODO: Given numpoints, generate n int arrays of size n-1 (.
-	// 		 
+    // TODO: Given numpoints, generate n int arrays of size n
 
-	int graph[numpoints][numpoints];
+    int graph[numpoints][numpoints];
 
 }
