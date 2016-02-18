424a8828edf969ee7a0a38303b1ff0cb43eeed44
KYLE KWONG <kylekwong@college.harvard.edu>
2016-02-17 23:42:12 -0500

    some progress?

--- randmst.c
+++ randmst.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+// tuple structure
+typedef struct {
+	int x;
+	int y;
+	int z;
+	int a;
+} coord;
+
 int main(int argc, char* argv[]) {
 	if (argc != 5) {
 		printf("Usage: randmst 0 numpoints numtrials dimension\n");
 		return 1;
 	}
+
+	// TODO: Given numpoints, generate n int arrays of size n-1 (.
+	// 		 
+
+	int graph[numpoints][numpoints];
+
 }
