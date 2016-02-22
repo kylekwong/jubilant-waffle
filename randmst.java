0d37e869ae82da9d3edc8769927991b98e7a41c4
KYLE KWONG <kylekwong@college.harvard.edu>
2016-02-21 21:53:00 -0500

    possibly done??

--- randmst.java
+++ randmst.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.lang.Math;
+import java.util.Arrays;
 
 public class randmst {
 	public static void main(String args[]) {
@@ -17,6 +18,8 @@ public class randmst {
 		int numtrials = Integer.parseInt(args[2]);
 		int dimension = Integer.parseInt(args[3]);
 
+		// add to final weight of MST
+		double weight = 0.0;
 		AdjacencyList adjacencyList = new AdjacencyList(numpoints);
 
 		adjacencyList = adjacencyList.init(numpoints, dimension);
@@ -27,6 +30,7 @@ public class randmst {
 
 		// s[0] == true means vertex 0 is in S
 		boolean[] s = new boolean[numpoints];
+		Arrays.fill(s, false);
 
 		MinBHeap heap = new MinBHeap(numpoints);
 		heap.insert(new Vertex(0, 0.0));
@@ -42,13 +46,15 @@ public class randmst {
 
 			// adds v to S
 			s[v.v] = true;
+			weight += v.edge;
+
 			List<Vertex> edges_of_v = adjacencyList.getEdge(v.v);
 
-			for(int i = 0; i < numpoints; ++i) {
+			for(int i = 0; i < numpoints-1; ++i) {
 				// if i not in S
 				if (!s[i]) {
-					if (dist[i] > edges_of_v.get(i)) {
-						dist[i] = edges_of_v.get(i);
+					if (dist[i] > (edges_of_v.get(i)).edge) {
+						dist[i] = (edges_of_v.get(i)).edge;
 						prev[i] = i;
 						/* Insert w into heap */
 						heap.insert(new Vertex(i, dist[i]));
@@ -56,5 +62,8 @@ public class randmst {
 				}
 			}
 		}
+
+		System.out.println(weight);
+
 	}
 }
